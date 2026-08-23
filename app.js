282046bafcd7d5c33e066813c806fa0b54c9634d
Thomas Nguyen <thnguyen2802@gmail.com>
2026-08-23 16:52:36 +0200

    skeleton-ish initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+let answer = ''
+
+function changeAnswer(newAnswer) {
+    answer = newAnswer
+}
+
+function checkAnswer() {
+
+}
+
+
+async function loadData(path) {
+    const response = await fetch(path)
+
+    if (!response.ok) {
+    throw new Error(`HTTP error: ${response.status}`)
+    }
+
+    return await response.json()
+}
+
+loadData('./voices.json').then((data) => {
+    const brawlerSelectElement = document.getElementById('brawlerSelect')
+    const brawlerNames = Object.keys(data)
+    brawlerNames.forEach(name => {
+        const imgElement = document.createElement('img')
+        imgElement.src = `./assets/${name}/icon.webp`
+        brawlerSelectElement.appendChild(imgElement)
+    })
+})
+
+
+
+
+
